a75fed2d1f4fd44f75986297cdc5a19169a76e02
tranngochung29200333333-commits <tranngochung29200333333@gmail.com>
2026-09-16 13:29:20 +0700

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://aowdfgemtvkmrvnuuctw.supabase.co";
+const SUPABASE_KEY = "sb_publishable_Dn-2lmgn3YfKteVJXDEoSA_dj9GmSVF";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
