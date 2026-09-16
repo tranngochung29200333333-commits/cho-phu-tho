46ec1e09d6e881e45c116ff90ddaecdcaaa8640e
tranngochung29200333333-commits <tranngochung29200333333@gmail.com>
2026-09-16 19:25:02 +0700

    Remove broken client-side view counter loader

--- js/supabase.js
+++ js/supabase.js
@@ -1,11 +1,3 @@
 const SUPABASE_URL = "https://aowdfgemtvkmrvnuuctw.supabase.co";
 const SUPABASE_KEY = "sb_publishable_Dn-2lmgn3YfKteVJXDEoSA_dj9GmSVF";
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
-(function loadSharedScripts(){
-    const scripts = [['js/view-counter.js','view-counter-loader']];
-    for (const [src,key] of scripts) {
-        if (document.querySelector(`script[data-loader="${key}"]`)) continue;
-        const s=document.createElement('script');s.src=src;s.dataset.loader=key;document.head.appendChild(s);
-    }
-})();
